9be97c12bb938bba91edf117eda08b483defc2d9
Karla Rangel <krangelhdz@gmail.com>
2024-11-26 11:25:17 +0000

    completed the add test, required the add function + notes

--- TestingInJS/add.test.js
+++ TestingInJS/add.test.js
@@ -6,10 +6,18 @@ describe("add", () => {});
 
 // When writing tests, we need to think what is it we want to test or verify. If we wanted to see in the REPL if the 'add' function is working
 // we'd type: add(2,2) and this'd return 4.
-// to translate it into a proper test case we use the 'it' function:
+// to translate it into a proper test case we use the 'it' function and use Jest matchers (expect().toBe() ):
+
+const add = require("./add.js");
 
 describe("add", () => {
 	it("adds 2 and 2", () => {
 		expect(2 + 2).toBe(4);
 	});
+	it("adds 0 and 5", () => {
+		expect(0 + 5).toBe(5);
+	});
 });
+
+// Exercise: write/implement the function 'add' into file add.js to satisfy the test above. Export the add function & require it in add.test.js (line 11).
+//           To see if the test pass, run 'Jest' in the command line.
