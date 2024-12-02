cbf7f9b1a8759499db0bb3823b57159c0aa3aba1
Karla Rangel <krangelhdz@gmail.com>
2024-12-02 12:39:22 +0000

    wrote test 5

--- TDDcomplexFunction/searchCandies.test.js
+++ TDDcomplexFunction/searchCandies.test.js
@@ -1,20 +1,23 @@
 const searchCandies = require("./searchCandies.js");
 
 describe("searchCandies", () => {
-	it("returns a list of candy names (lowercased) that start with the searchStr (lowercased) and candy price is less than maxPrice", () => {
+	it("returns a list of candy names that start with the searchStr and candy price is less than maxPrice", () => {
 		expect(searchCandies("Ma", 10)).toEqual(["Mars", "Maltesers"]);
 	});
-	it("returns a list of candy names (lowercased) that start with the searchStr (lowercased) and candy price is less than maxPrice", () => {
+	it("returns a list of candy names that start with the searchStr and candy price is less than maxPrice", () => {
 		expect(searchCandies("Ma", 2)).toEqual(["Mars"]);
 	});
-	it("returns a list of candy names (lowercased) that start with the searchStr (lowercased) and candy price is less than maxPrice", () => {
+	it("returns a list of candy names that start with the searchStr and candy price is less than maxPrice", () => {
 		expect(searchCandies("S", 10)).toEqual([
 			"Skitties",
 			"Skittles",
 			"Starburst",
 		]);
 	});
-	it("returns a list of candy names (lowercased) that start with the searchStr (lowercased) and candy price is less than maxPrice", () => {
+	it("returns a list of candy names that start with the searchStr and candy price is less than maxPrice", () => {
 		expect(searchCandies("S", 4)).toEqual(["Skitties", "Skittles"]);
+	});
+	it("returns a list of candy names that start with the searchStr and candy price is less than maxPrice", () => {
+		expect(searchCandies("ma", 10)).toEqual(["Mars", "Maltesers"]);
 	});
 });
