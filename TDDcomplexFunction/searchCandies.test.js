589d3ee5363fa930c7143ffde06b07dc8b7a3ff3
Karla Rangel <krangelhdz@gmail.com>
2024-12-02 12:24:59 +0000

    required the searchCandies function & wrote 1 test

--- TDDcomplexFunction/searchCandies.test.js
+++ TDDcomplexFunction/searchCandies.test.js
@@ -1,1 +1,7 @@
-//
+const searchCandies = require("./searchCandies.js");
+
+describe("searchCandies", () => {
+	it("checks array, converts to lowercase the candy name and provided searchStr and returns a list of candy names that start the searchStr and candy price is less than maxPrice", () => {
+		expect(searchCandies("Ma", 10)).toEqual(["Mars", "Maltesers"]);
+	});
+});
